b362bf82c703ec550db3a0b55d091f1872e17066
Gus <august.colonna@gmail.com>
2023-08-23 11:59:42 +0200

    restructure json file of classes to make it easier to map and add classes to profile

--- src/components/FitnessClassSchedule.jsx
+++ src/components/FitnessClassSchedule.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { useState } from "react";
 
 function FitnessClassSchedule(props) {
-	const { session } = props;
+	const { day, selectSpecificDay } = props;
+	const [showClasses, setShowClasses] = useState(false);
+
+	const toggleClassSchedule = () => {
+		setShowClasses(!showClasses);
+	};
 
 	return (
 		<div className="daily-schedule">
-			<h2>{session.title}</h2>
-			<p>{session.time}</p>
+			<h2>{day.day}</h2>
+			<button onClick={() => {
+				selectSpecificDay(day.day)
+				toggleClassSchedule();
+			}}>{showClasses ? "Less" : "More" }</button>
+		{showClasses ? (
+			<div>
+				<ul>
+					<li>{day.title}</li>
+					<li>{day.time}</li>
+				</ul>
+			</div>
+		) : (
+			""
+		)}
 		</div>
+		
 	);
 }
 
