334c30f40c75dd6b4fd26e871936e5726964e8ea
August Colonna <august.colonna@gmail.com>
2023-08-30 11:17:52 +0200

    CRUD applications for adding workouts to  your personal calendar

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { UserAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { db } from "../firebase";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
@@ -11,9 +14,32 @@ import "../stylesheets/profile.scss";
 function Profile() {
   const { user, logout } = UserAuth();
   const navigate = useNavigate();
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [myWorkouts, setMyWorkouts] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+  const workoutsCollection = collection(db, "myWorkouts");
+
+  useEffect(() => {
+    const getWorkouts = async () => {
+      try {
+        const data = await getDocs(workoutsCollection);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        console.log(filteredData);
+        setMyWorkouts(filteredData);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    getWorkouts();
+  }, []);
+
+  const cancelWorkout = async (id) => {
+    const workoutDoc = doc(db, "myWorkouts", id);
+    await deleteDoc(workoutDoc);
+  };
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -81,6 +107,20 @@ function Profile() {
         </div>
         <div className="gym-news">
           <h2>iframe instagram page here</h2>
+        </div>
+        <div>
+          <h2>My Workouts this Week</h2>
+          {myWorkouts.map((workout) => {
+            return (
+              <div key={workout.id}>
+                <h3>{workout.title}</h3>
+                <p>{workout.time}</p>
+                <button onClick={() => cancelWorkout(workout.id)}>
+                  Cancel
+                </button>
+              </div>
+            );
+          })}
         </div>
       </div>
       <NavBar />
